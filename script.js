f3571551c4ffcd4df55d790fd943d18a333867d7
David van der Westhuizen <dcjwest@gmail.com>
2019-02-01 15:13:52 +0200

    Splash screen :sweat_drops:
    First attempt at adding a splash screen to hide my website while it loads. Simply used a fullscreen div with a gif, some text and a fade out keyframes animation instantiated with JS. Did some more "code house-cleaning" as well. Also included the minified jQuery CDN instead of hosting the full file to try speeding up load times.

--- script.js
+++ script.js
@@ -7,37 +7,7 @@ Copyright (c) 2018 David van der Westhuizen
 */
 
 $(function(){
-	// Tooltip pop-up events
-	$('#emailBtn').on('click', updateToolTip);
-	$('#email').on('mouseleave', resetToolTip);
 
-	// Show / Hide tooltip functions
-	function updateToolTip(event){
-		copyToClipboard();
-		$('.tooltip').css({'background-color':'#A5FF7F', 'color': '#000'}).html('Yay! My email address has been copied to your clipboard &#10004;');
-		$('.tooltip').addClass('clicked');
-		event.preventDefault();
-	}
-
-	function resetToolTip(event){
-		$('.tooltip').css({'background-color':'#000', 'color': '#fff'}).html('Click to copy my email address to your clipboard! &#9786;');
-		$('.tooltip').removeClass('clicked');
-		event.preventDefault();
-	}
-
-	//Copy email address to clipboard after clicking email icon
-	function copyToClipboard(){
-		$('#myEmail').select();
-		document.execCommand("copy");
-	}
-
-	// Move scroll position to top of page after refresh
-	window.onbeforeunload = function () {
-	  window.scrollTo(0, 0);
-	}
-});
-
-$(window).on('load', function() {
 	// Initialise slideshow variables
 	var proPic = $('#slider');
 	var picArr = [
@@ -67,4 +37,28 @@ $(window).on('load', function() {
 		proPic.fadeIn(400);
 	}
 	setInterval(togglePic, 4000);
+
+	// Tooltip pop-up events
+	$('#emailBtn').on('click', updateToolTip);
+	$('#email').on('mouseleave', resetToolTip);
+
+	// Show / Hide tooltip functions
+	function updateToolTip(event){
+		copyToClipboard();
+		$('.tooltip').css({'background-color':'#A5FF7F', 'color': '#000'}).html('Yay! My email address has been copied to your clipboard &#10004;');
+		$('.tooltip').addClass('clicked');
+		event.preventDefault();
+	}
+
+	function resetToolTip(event){
+		$('.tooltip').css({'background-color':'#000', 'color': '#fff'}).html('Click to copy my email address to your clipboard! &#9786;');
+		$('.tooltip').removeClass('clicked');
+		event.preventDefault();
+	}
+
+	//Copy email address to clipboard after clicking email icon
+	function copyToClipboard(){
+		$('#myEmail').select();
+		document.execCommand("copy");
+	}
 });
